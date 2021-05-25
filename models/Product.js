78cf3760fc0cd43284c15fe49647bfa6ea2c04b5
Willie <write2willie@gmail.com>
2021-05-24 21:49:13 -0400

    updated inventory into product

---
+++ models/Product.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Product extends Model {}
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    }, 
+    stock: {
+      type: DataTypes.INTEGER(100),
+      allowNull: true,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+  }
+);
+
+module.exports = Product;
