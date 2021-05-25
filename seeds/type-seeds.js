9c4f16f2e326d060e978ad45f36457f97e3b3f27
Scott Erwin <scottallenerwin@gmail.com>
2021-05-25 14:29:07 -0400

    updated Type-Seeds with production seeds

--- seeds/type-seeds.js
+++ seeds/type-seeds.js
@@ -1,17 +1,32 @@
-const { Type } = require('../models');
+const { Type } = require("../models");
 
 const typeData = [
   {
-    type_name: 'High-top',
+    type_name: "Retro-Sneaker",
   },
   {
-    type_name: 'High-top-Boot',
+    type_name: "High-Top-Sneaker",
   },
   {
-    type_name: 'Iow-Top',
+    type_name: "High-Top-Boot",
   },
   {
-    type_name: 'flip-flops',
+    type_name: "Low-Top-Sneaker",
+  },
+  {
+    type_name: "High-top-Canvas-White",
+  },
+  {
+    type_name: "High-top-Canvas-Black",
+  },
+  {
+    type_name: "flip-flops",
+  },
+  {
+    type_name: "Slide-Shoes",
+  },
+  {
+    type_name: "Toms-Shoes",
   },
 ];
 
