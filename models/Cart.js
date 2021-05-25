939192b0a074901b311345aad56bd12b299d0aee
Willie <write2willie@gmail.com>
2021-05-24 22:00:26 -0400

    updated cart columns from item and size to quantity

--- models/Cart.js
+++ models/Cart.js
@@ -11,25 +11,10 @@ Cart.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    item: {
+    quantity: {
       type: DataTypes.INTEGER(100),
       allowNull: false,
-      // references: {
-      //   model: "Inventory",
-      //   key: "id",
-      // },
     },
-    size: {
-      type: DataTypes.INTEGER(5),
-      allowNull: false,
-    },
-    // design: {
-    //   allowNull: false,
-    //   references: {
-    //     model: "Design",
-    //     key: "id",
-    //   },
-    // },
   },
   {
     sequelize,
