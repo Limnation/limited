ac25a8b500fc9a3c5856464acc0658e1137c46e2
sineusw <sineusw@gmail.com>
2021-05-26 19:45:14 -0400

    created find all and find by id routes for types

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes");
 const cartRoutes = require("./cart-routes");
 const productRoutes = require("./product-routes");
+const typeRoutes = require("./type-routes");
 
 router.use("/users", userRoutes);
 router.use("/carts", cartRoutes);
 router.use("/products", productRoutes);
+router.use("/types", typeRoutes);
 
 module.exports = router;
--- routes/api/type-routes.js
+++ routes/api/type-routes.js
@@ -1,1 +1,25 @@
 const router = require("express").Router();
+const { Type } = require("../../models");
+
+router.get("/", async (req, res) => {
+    try {
+        const typeData = await Type.findAll({
+
+        })
+        console.log(typeData);
+        res.status(200).json(typeData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const typeData = await Type.findByPk(req.params.id);
+        
+        res.status(200).json(typeData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+module.exports = router;
