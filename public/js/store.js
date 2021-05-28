0486253b90019ccbb0ad1abb68880007dea58cf4
elgalloloco <zreggert@gmail.com>
2021-05-28 19:28:05 -0400

    function to render all products on store page finished

---
+++ public/js/store.js
@@ -0,0 +1,39 @@
+const getProducts = () => {
+
+    fetch('/api/products', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        renderProducts(data);
+    });
+};
+
+
+function renderProducts(data) {
+    let products= [];
+    for ( var i = 0; i < data.length; i++ ) {
+        products.push(data[i]); 
+    }
+    products.forEach(product => {
+        $(".products").append(generateHtml(product));
+    })
+}
+
+
+function generateHtml(product) {
+    return `
+    <div class="product">
+        <img src="${product.image_name}" alt="image of ${product.product_name}" />
+        <p class="product-name">${product.product_name}</p>
+        <p class="product-price">${product.price}</>
+    </div>`
+}
+
+
+getProducts();
