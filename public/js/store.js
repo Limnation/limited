3be0045b45bbe203523b584db1227beea7530eb5
elgalloloco <zreggert@gmail.com>
2021-05-28 22:37:40 -0400

    started working on the filters for types

--- public/js/store.js
+++ public/js/store.js
@@ -10,30 +10,99 @@ const getProducts = () => {
         return response.json();
     })
     .then(function (data) {
-        renderProducts(data);
+        renderAllProducts(data);
     });
 };
 
+const getTypes = () => {
 
-function renderProducts(data) {
+    fetch('/api/types', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        renderAllTypes(data);
+    });
+};
+
+function renderAllProducts(data) {
     let products= [];
     for ( var i = 0; i < data.length; i++ ) {
         products.push(data[i]); 
     }
     products.forEach(product => {
-        $(".products").append(generateHtml(product));
-    })
-}
+        $(".products").append(generateProductsHtml(product));
+    });
+};
 
+function renderAllTypes(data) {
+    let types = [];
+    for ( var i = 0; i < data.length; i++ ) {
+        types.push(data[i]);
+    }
+    types.forEach(type => {
+        $(".ul-footwear").append(generateTypesHtml(type));
+    });
+};
 
-function generateHtml(product) {
+//<img src="images/store/${product.image_name}.png" alt="image of ${product.image_name}" />
+function generateProductsHtml(product) {
     return `
     <div class="product">
         <img src="images/store/Product-image.png" alt="image of ${product.image_name}" />
         <p class="product-name">${product.product_name}</p>
         <p class="product-price">${product.price}</>
     </div>`
+};
+
+function generateTypesHtml(type) {
+    return `
+    <li class="li-footwear" value="${type.id}"><button type="button" class="li-button">${type.type_name}</button></li>`
+};
+
+
+
+$(document).ready(function() {
+    $(".li-button").click(function() {
+
+        alert('The button was clicked');
+
+        console.log("button clicked");
+        filterTypes();
+    });
+
+    const filterTypes = () => {
+
+        console.log('heard the click');
+        // console.log(value);
+        // fetch(`api/searches/${value}`, {
+        //     method: 'GET',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     }
+        // })
+        // .then(function (response) {
+        //     return response.json();
+        // })
+        // .then(function (data) {
+        //     renderType(data);
+        //     console.log(data);
+        // });
+    
+    };
+});
+
+
+function init() {
+    getProducts();
+    getTypes()
 }
 
 
-getProducts();
+
+init();
